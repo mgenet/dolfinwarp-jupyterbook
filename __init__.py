f79e214f0aae5034a429adaf88bcf3e800a307b5
Cécile Patte <cecile.patte@inria.fr>
2019-09-05 16:28:46 +0200

    change

--- __init__.py
+++ __init__.py
@@ -6,7 +6,6 @@ from .Material_Inelastic_Growth_StrainDriven import *
 from .Material_Elastic_Kirchhoff import *
 from .Material_Inelastic_Relaxation import *
 from .Material_Elastic_Bulk_Pneumo import *
-from .Material_Elastic_Tawhai import *
 from .Material_Elastic_Bulk_CiarletGeymonat import *
 from .Loading import *
 from .SubSol import *
