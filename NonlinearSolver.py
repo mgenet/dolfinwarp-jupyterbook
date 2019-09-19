mgenet/dolfinwarp-jupyterbook

commit 08915c0f69a3cb44fbc9b3d166a3a4984829101a

Implementation of point-wise directional constraints through penalization

NonlinearSolver.py
@@ -218,11 +218,44 @@ class NonlinearSolver():
             bcs=[constraint.bc for constraint in self.constraints],
             A_tensor=self.jac_mat,
             b_tensor=self.res_vec,
+            add_values=False,
+            finalize_tensor=False,
+            form_compiler_parameters=self.problem.form_compiler_parameters)
+        timer = time.time() - timer
+        self.printer.print_str(" "+str(timer)+" s",tab=False,newline=False)
+        # self.printer.print_var("res_vec",self.res_vec.get_local())
+        # self.printer.print_var("jac_mat",self.jac_mat.array())
+        Pi = dolfin.Constant(0.) * self.problem.dV
+        for loading in self.problem.directional_penalties:
+            Pi += (loading.val/2) * dolfin.inner(
+                self.problem.subsols["U"].subfunc,
+                loading.N)**2 * loading.measure
+        res_form = dolfin.derivative(
+            Pi,
+            self.problem.sol_func,
+            self.problem.dsol_test)
+        jac_form = dolfin.derivative(
+            res_form,
+            self.problem.sol_func,
+            self.problem.dsol_tria)
+        dolfin.assemble( #MG20190513: Cannot use point integral within assemble_system
+           -res_form,
+            tensor=self.res_vec,
+            add_values=True,
+            finalize_tensor=True,
+            form_compiler_parameters=self.problem.form_compiler_parameters)
+        timer = time.time() - timer
+        self.printer.print_str(" "+str(timer)+" s",tab=False,newline=False)
+        # self.printer.print_var("res_vec",self.res_vec.get_local())
+        dolfin.assemble( #MG20190513: Cannot use point integral within assemble_system
+            jac_form,
+            tensor=self.jac_mat,
+            add_values=True,
+            finalize_tensor=True,
             form_compiler_parameters=self.problem.form_compiler_parameters)
         timer = time.time() - timer
         self.printer.print_str(" "+str(timer)+" s",tab=False)
         # self.printer.print_var("jac_mat",self.jac_mat.array())
-        # self.printer.print_var("res_vec",self.res_vec.get_local())
 
         if not (numpy.isfinite(self.res_vec).all()):
             self.printer.print_str("Warning! Residual is NaN!")

Loading.py
@@ -22,7 +22,8 @@ class Loading():
             measure,
             val=None,
             val_ini=None,
-            val_fin=None):
+            val_fin=None,
+            N=None):
 
         self.measure = measure
 
@@ -35,6 +36,9 @@ class Loading():
                 val_ini=val_ini,
                 val_fin=val_fin)
         self.val = self.tv_val.val
+
+        if (N is not None):
+            self.N = dolfin.Constant(N)
 
 
 

Problem.py
@@ -28,8 +28,9 @@ class Problem():
         self.inelastic_behaviors_mixed    = []
         self.inelastic_behaviors_internal = []
 
-        self.constraints = []
-        self.penalties   = []
+        self.constraints           = []
+        self.normal_penalties      = []
+        self.directional_penalties = []
 
         self.surface_tensions   = []
         self.surface0_loadings  = []
@@ -100,7 +101,8 @@ class Problem():
 
     def set_measures(self,
             domains=None,
-            boundaries=None):
+            boundaries=None,
+            points=None):
 
         if (domains is not None):
             self.dV = dolfin.Measure(
@@ -120,6 +122,16 @@ class Problem():
         else:
             self.dS = dolfin.Measure(
                 "ds",
+                domain=self.mesh)
+
+        if (points is not None):
+            self.dP = dolfin.Measure(
+                "dP",
+                domain=self.mesh,
+                subdomain_data=points)
+        else:
+            self.dP = dolfin.Measure(
+                "dP",
                 domain=self.mesh)
 
 
@@ -192,60 +204,6 @@ class Problem():
 
 
 
-    def get_subsol_function_space(self,
-            name):
-
-        index = self.subsols.keys().index(name)
-        # print str(name)+" index = "+str(index)
-        return self.sol_fs.sub(index)
-
-
-
-    def get_displacement_function_space(self):
-
-        if (len(self.subsols) == 1):
-            return self.sol_fs
-        else:
-            return self.get_subsol_function_space(name="U")
-
-
-
-    def get_unloaded_displacement_function_space(self):
-
-        assert (len(self.subsols) > 1)
-        return self.get_subsol_function_space(name="Up")
-
-
-
-    def get_pressure_function_space(self):
-
-        assert (len(self.subsols) > 1)
-        return self.get_subsol_function_space(name="P")
-
-
-
-    def get_unloaded_pressure_function_space(self):
-
-        assert (len(self.subsols) > 1)
-        return self.get_subsol_function_space(name="Pp")
-
-
-
-    def get_growth_function_space(self):
-
-        assert (len(self.subsols) > 1)
-        return self.get_subsol_function_space(name="thetag")
-        #return self.get_subsol_function_space(name="Fg")
-
-
-
-    def get_relaxation_function_space(self):
-
-        assert (len(self.subsols) > 1)
-        return self.get_subsol_function_space(name="Fr")
-
-
-
     def set_solution_finite_element(self):
 
         if (len(self.subsols) == 1):
@@ -262,6 +220,15 @@ class Problem():
             self.mesh,
             self.sol_fe) # MG: element keyword don't work here…
         #print self.sol_fs
+
+
+
+    def get_subsol_function_space(self,
+            name):
+
+        index = self.subsols.keys().index(name)
+        # print str(name)+" index = "+str(index)
+        return self.sol_fs.sub(index)
 
 
 
@@ -451,14 +418,26 @@ class Problem():
 
 
 
-    def add_penalty(self,
+    def add_normal_penalty(self,
             *args,
             **kwargs):
 
         loading = dcm.Loading(
             *args,
             **kwargs)
-        self.penalties += [loading]
+        self.normal_penalties += [loading]
+        return loading
+
+
+
+    def add_directional_penalty(self,
+            *args,
+            **kwargs):
+
+        loading = dcm.Loading(
+            *args,
+            **kwargs)
+        self.directional_penalties += [loading]
         return loading
 
 
@@ -578,85 +557,6 @@ class Problem():
             **kwargs)
         self.qois += [qoi]
         return qoi
-
-
-
-    def add_strain_qois(self,
-            strain_type="elastic",
-            configuration_type="loaded"):
-
-        if (configuration_type == "loaded"):
-            kin = self.kinematics
-        elif (configuration_type == "unloaded"):
-            kin = self.unloaded_kinematics
-
-        if (strain_type == "elastic"):
-            basename = "E^e_"
-            strain = kin.Ee
-        elif (strain_type == "total"):
-            basename = "E^t_"
-            strain = kin.Et
-
-        self.add_qoi(
-            name=basename+"XX",
-            expr=strain[0,0] * self.dV)
-        if (self.dim >= 2):
-            self.add_qoi(
-                name=basename+"YY",
-                expr=strain[1,1] * self.dV)
-            if (self.dim >= 3):
-                self.add_qoi(
-                    name=basename+"ZZ",
-                    expr=strain[2,2] * self.dV)
-        if (self.dim >= 2):
-            self.add_qoi(
-                name=basename+"XY",
-                expr=strain[0,1] * self.dV)
-            if (self.dim >= 3):
-                self.add_qoi(
-                    name=basename+"YZ",
-                    expr=strain[1,2] * self.dV)
-                self.add_qoi(
-                    name=basename+"ZX",
-                    expr=strain[2,0] * self.dV)
-
-
-
-    def add_stress_qois(self,
-            stress_type="cauchy"):
-
-        if (stress_type in ("cauchy", "sigma")):
-            basename = "s_"
-            stress = self.sigma
-        elif (stress_type in ("piola", "PK2", "Sigma")):
-            basename = "S_"
-            stress = self.Sigma
-        elif (stress_type in ("PK1", "P")):
-            basename = "P_"
-            stress = self.PK1
-
-        self.add_qoi(
-            name=basename+"XX",
-            expr=stress[0,0] * self.dV)
-        if (self.dim >= 2):
-            self.add_qoi(
-                name=basename+"YY",
-                expr=stress[1,1] * self.dV)
-            if (self.dim >= 3):
-                self.add_qoi(
-                    name=basename+"ZZ",
-                    expr=stress[2,2] * self.dV)
-        if (self.dim >= 2):
-            self.add_qoi(
-                name=basename+"XY",
-                expr=stress[0,1] * self.dV)
-            if (self.dim >= 3):
-                self.add_qoi(
-                    name=basename+"YZ",
-                    expr=stress[1,2] * self.dV)
-                self.add_qoi(
-                    name=basename+"ZX",
-                    expr=stress[2,0] * self.dV)
 
 
 

Problem_Hyperelasticity.py
@@ -142,7 +142,7 @@ class HyperelasticityProblem(Problem):
 
 
     def set_solution_degree(self,
-            U_degree=1):
+            U_degree=1): #MG20190513: Should have different name, right?
 
         self.set_subsols(
             U_degree=U_degree)
@@ -160,6 +160,51 @@ class HyperelasticityProblem(Problem):
         self.set_foi_finite_elements_DG(
             degree=U_degree-1)
         self.set_foi_function_spaces()
+
+
+
+    def get_displacement_function_space(self):
+
+        if (len(self.subsols) == 1):
+            return self.sol_fs
+        else:
+            return self.get_subsol_function_space(name="U")
+
+
+
+    def get_unloaded_displacement_function_space(self):
+
+        assert (len(self.subsols) > 1)
+        return self.get_subsol_function_space(name="Up")
+
+
+
+    def get_pressure_function_space(self):
+
+        assert (len(self.subsols) > 1)
+        return self.get_subsol_function_space(name="P")
+
+
+
+    def get_unloaded_pressure_function_space(self):
+
+        assert (len(self.subsols) > 1)
+        return self.get_subsol_function_space(name="Pp")
+
+
+
+    def get_growth_function_space(self):
+
+        assert (len(self.subsols) > 1)
+        return self.get_subsol_function_space(name="thetag")
+        #return self.get_subsol_function_space(name="Fg")
+
+
+
+    def get_relaxation_function_space(self):
+
+        assert (len(self.subsols) > 1)
+        return self.get_subsol_function_space(name="Fr")
 
 
 
@@ -303,7 +348,8 @@ class HyperelasticityProblem(Problem):
 
 
     def set_variational_formulation(self,
-            penalties=[],
+            normal_penalties=[],
+            directional_penalties=[],
             surface_tensions=[],
             surface0_loadings=[],
             pressure0_loadings=[],
@@ -313,19 +359,32 @@ class HyperelasticityProblem(Problem):
             volume_loadings=[],
             dt=None):
 
-        # self.Pi = self.Psi * self.dV
+        self.Pi = self.Psi * self.dV
 
-        # for loading in surface0_loadings:
-        #     self.Pi -= dolfin.inner(
-        #         loading.val,
-        #         self.subsols["U"].subfunc) * loading.measure
+        if (self.w_unloaded_configuration):
+            self.Pi += self.unloaded_Psi * self.dV
 
-        # for loading in volume0_loadings:
-        #     self.Pi -= dolfin.inner(
-        #         loading.val,
-        #         self.subsols["U"].subfunc) * loading.measure
+        for loading in normal_penalties:
+            self.Pi += (loading.val/2) * dolfin.inner(
+                self.subsols["U"].subfunc,
+                self.mesh_normals)**2 * loading.measure
 
-        self.Pi = dolfin.Constant(0.) * self.dV
+        if (self.w_unloaded_configuration):
+            for loading in normal_penalties:
+                self.Pi += (loading.val/2) * dolfin.inner(
+                    self.subsols["Up"].subfunc,
+                    self.mesh_normals)**2 * loading.measure
+
+        # for loading in directional_penalties: #MG20190513: Cannot use point integral within assemble_system
+        #     self.Pi += (loading.val/2) * dolfin.inner(
+        #         self.subsols["U"].subfunc,
+        #         loading.N)**2 * loading.measure
+        #
+        # if (self.w_unloaded_configuration):
+        #     for loading in directional_penalties: #MG20190513: Cannot use point integral within assemble_system
+        #         self.Pi += (loading.val/2) * dolfin.inner(
+        #             self.subsols["Up"].subfunc,
+        #             loading.N)**2 * loading.measure
 
         for loading in surface_tensions:
             FmTN = dolfin.dot(
@@ -336,65 +395,80 @@ class HyperelasticityProblem(Problem):
                 FmTN))
             self.Pi += loading.val * self.kinematics.Jt * T * loading.measure
 
+        for loading in surface0_loadings:
+            self.Pi -= dolfin.inner(
+                loading.val,
+                self.subsols["U"].subfunc) * loading.measure
+
+        for loading in pressure0_loadings:
+            self.Pi -= dolfin.inner(
+               -loading.val * self.mesh_normals,
+                self.subsols["U"].subfunc) * loading.measure
+
+        for loading in volume0_loadings:
+            self.Pi -= dolfin.inner(
+                loading.val,
+                self.subsols["U"].subfunc) * loading.measure
+
         self.res_form = dolfin.derivative(
             self.Pi,
             self.sol_func,
             self.dsol_test); assert (self.w_growth != "mixed") and (self.w_relaxation != "mixed")
 
-        self.res_form = dolfin.inner(
-            self.Sigma,
-            dolfin.derivative(
-                self.kinematics.Et,
-                self.subsols["U"].subfunc,
-                self.subsols["U"].dsubtest)) * self.dV
+        # self.res_form += dolfin.inner(
+        #     self.Sigma,
+        #     dolfin.derivative(
+        #         self.kinematics.Et,
+        #         self.subsols["U"].subfunc,
+        #         self.subsols["U"].dsubtest)) * self.dV
+        #
+        # if (self.w_incompressibility):
+        #     self.res_form += dolfin.inner(
+        #         self.kinematics.Je-1,
+        #         self.subsols["P"].dsubtest) * self.dV
 
-        if (self.w_incompressibility):
-            self.res_form += dolfin.inner(
-                self.kinematics.Je-1,
-                self.subsols["P"].dsubtest) * self.dV
+        # if (self.w_unloaded_configuration):
+        #     self.res_form += dolfin.inner(
+        #         self.unloaded_Sigma,
+        #         dolfin.derivative(
+        #             self.unloaded_kinematics.Et,
+        #             self.subsols["Up"].subfunc,
+        #             self.subsols["Up"].dsubtest)) * self.dV
+        #
+        #     if (self.w_incompressibility):
+        #         self.res_form += dolfin.inner(
+        #             self.unloaded_kinematics.Je-1,
+        #             self.subsols["Pp"].dsubtest) * self.dV
 
-        if (self.w_unloaded_configuration):
-            self.res_form += dolfin.inner(
-                self.unloaded_Sigma,
-                dolfin.derivative(
-                    self.unloaded_kinematics.Et,
-                    self.subsols["Up"].subfunc,
-                    self.subsols["Up"].dsubtest)) * self.dV
+        # for loading in normal_penalties:
+        #     self.res_form += loading.val * dolfin.inner(
+        #         self.subsols["U"].subfunc,
+        #         self.mesh_normals) * dolfin.inner(
+        #         self.subsols["U"].dsubtest,
+        #         self.mesh_normals) * loading.measure
+        #
+        # if (self.w_unloaded_configuration):
+        #     for loading in normal_penalties:
+        #         self.res_form += loading.val * dolfin.inner(
+        #             self.subsols["Up"].subfunc,
+        #             self.mesh_normals) * dolfin.inner(
+        #             self.subsols["Up"].dsubtest,
+        #             self.mesh_normals) * loading.measure
 
-            if (self.w_incompressibility):
-                self.res_form += dolfin.inner(
-                    self.unloaded_kinematics.Je-1,
-                    self.subsols["Pp"].dsubtest) * self.dV
-
-        for loading in penalties:
-            self.res_form += loading.val * dolfin.inner(
-                self.subsols["U"].subfunc,
-                self.mesh_normals) * dolfin.inner(
-                self.subsols["U"].dsubtest,
-                self.mesh_normals) * loading.measure
-
-        if (self.w_unloaded_configuration):
-            for loading in penalties:
-                self.res_form += loading.val * dolfin.inner(
-                    self.subsols["Up"].subfunc,
-                    self.mesh_normals) * dolfin.inner(
-                    self.subsols["Up"].dsubtest,
-                    self.mesh_normals) * loading.measure
-
-        for loading in surface0_loadings:
-            self.res_form -= dolfin.inner(
-                loading.val,
-                self.subsols["U"].dsubtest) * loading.measure
-
-        for loading in pressure0_loadings:
-            self.res_form -= dolfin.inner(
-               -loading.val * self.mesh_normals,
-                self.subsols["U"].dsubtest) * loading.measure
-
-        for loading in volume0_loadings:
-            self.res_form -= dolfin.inner(
-                loading.val,
-                self.subsols["U"].dsubtest) * loading.measure
+        # for loading in surface0_loadings:
+        #     self.res_form -= dolfin.inner(
+        #         loading.val,
+        #         self.subsols["U"].dsubtest) * loading.measure
+        #
+        # for loading in pressure0_loadings:
+        #     self.res_form -= dolfin.inner(
+        #        -loading.val * self.mesh_normals,
+        #         self.subsols["U"].dsubtest) * loading.measure
+        #
+        # for loading in volume0_loadings:
+        #     self.res_form -= dolfin.inner(
+        #         loading.val,
+        #         self.subsols["U"].dsubtest) * loading.measure
 
         for loading in surface_loadings:
             FmTN = dolfin.dot(
@@ -429,3 +503,82 @@ class HyperelasticityProblem(Problem):
             self.res_form,
             self.sol_func,
             self.dsol_tria)
+
+
+
+    def add_strain_qois(self,
+            strain_type="elastic",
+            configuration_type="loaded"):
+
+        if (configuration_type == "loaded"):
+            kin = self.kinematics
+        elif (configuration_type == "unloaded"):
+            kin = self.unloaded_kinematics
+
+        if (strain_type == "elastic"):
+            basename = "E^e_"
+            strain = kin.Ee
+        elif (strain_type == "total"):
+            basename = "E^t_"
+            strain = kin.Et
+
+        self.add_qoi(
+            name=basename+"XX",
+            expr=strain[0,0] * self.dV)
+        if (self.dim >= 2):
+            self.add_qoi(
+                name=basename+"YY",
+                expr=strain[1,1] * self.dV)
+            if (self.dim >= 3):
+                self.add_qoi(
+                    name=basename+"ZZ",
+                    expr=strain[2,2] * self.dV)
+        if (self.dim >= 2):
+            self.add_qoi(
+                name=basename+"XY",
+                expr=strain[0,1] * self.dV)
+            if (self.dim >= 3):
+                self.add_qoi(
+                    name=basename+"YZ",
+                    expr=strain[1,2] * self.dV)
+                self.add_qoi(
+                    name=basename+"ZX",
+                    expr=strain[2,0] * self.dV)
+
+
+
+    def add_stress_qois(self,
+            stress_type="cauchy"):
+
+        if (stress_type in ("cauchy", "sigma")):
+            basename = "s_"
+            stress = self.sigma
+        elif (stress_type in ("piola", "PK2", "Sigma")):
+            basename = "S_"
+            stress = self.Sigma
+        elif (stress_type in ("PK1", "P")):
+            basename = "P_"
+            stress = self.PK1
+
+        self.add_qoi(
+            name=basename+"XX",
+            expr=stress[0,0] * self.dV)
+        if (self.dim >= 2):
+            self.add_qoi(
+                name=basename+"YY",
+                expr=stress[1,1] * self.dV)
+            if (self.dim >= 3):
+                self.add_qoi(
+                    name=basename+"ZZ",
+                    expr=stress[2,2] * self.dV)
+        if (self.dim >= 2):
+            self.add_qoi(
+                name=basename+"XY",
+                expr=stress[0,1] * self.dV)
+            if (self.dim >= 3):
+                self.add_qoi(
+                    name=basename+"YZ",
+                    expr=stress[1,2] * self.dV)
+                self.add_qoi(
+                    name=basename+"ZX",
+                    expr=stress[2,0] * self.dV)

Problem_InverseHyperelasticity.py
@@ -69,7 +69,7 @@ class InverseHyperelasticityProblem(Problem):
 
 
     def set_solution_degree(self,
-            U_degree=1):
+            U_degree=1): #MG20190513: Should have different name, right?
 
         self.set_subsols(
             U_degree=U_degree)
@@ -132,7 +132,8 @@ class InverseHyperelasticityProblem(Problem):
 
 
     def set_variational_formulation(self,
-            penalties=[],
+            normal_penalties=[],
+            directional_penalties=[],
             surface_tensions=[],
             surface0_loadings=[],
             pressure0_loadings=[],

commit ce8736e80573623c7702d5f4907fa82c96a82797

Bug introduced in 08915c0f: Need to finalize tensors!

NonlinearSolver.py
@@ -220,53 +220,66 @@ class NonlinearSolver():
             self.res_old_norm = self.res_norm
 
         # linear system: Assembly
-        self.printer.print_str("Assembly…",newline=False)
-        timer = time.time()
-        self.jac_mat,self.res_vec = dolfin.assemble_system(
-            self.problem.jac_form,
-           -self.problem.res_form,
-            bcs=[constraint.bc for constraint in self.constraints],
-            A_tensor=self.jac_mat,
-            b_tensor=self.res_vec,
-            add_values=False,
-            finalize_tensor=False,
-            form_compiler_parameters=self.problem.form_compiler_parameters)
-        timer = time.time() - timer
-        self.printer.print_str(" "+str(timer)+" s",tab=False,newline=False)
-        # self.printer.print_var("res_vec",self.res_vec.get_local())
-        # self.printer.print_var("jac_mat",self.jac_mat.array())
+        if (len(self.problem.directional_penalties)): #MG20190513: Cannot use point integral within assemble_system
+            self.printer.print_str("Assembly…",newline=False)
+            timer = time.time()
+            dolfin.assemble_system(
+                self.problem.jac_form,
+               -self.problem.res_form,
+                bcs=[constraint.bc for constraint in self.constraints],
+                A_tensor=self.jac_mat,
+                b_tensor=self.res_vec,
+                add_values=False,
+                finalize_tensor=False,
+                form_compiler_parameters=self.problem.form_compiler_parameters)
+            timer = time.time() - timer
+            self.printer.print_str(" "+str(timer)+" s",tab=False)
+            # self.printer.print_var("res_vec",self.res_vec.get_local())
+            # self.printer.print_var("jac_mat",self.jac_mat.array())
 
-        Pi = dolfin.Constant(0.) * self.problem.dV
-        for loading in self.problem.directional_penalties:
-            Pi += (loading.val/2) * dolfin.inner(
-                self.problem.subsols["U"].subfunc,
-                loading.N)**2 * loading.measure
-        res_form = dolfin.derivative(
-            Pi,
-            self.problem.sol_func,
-            self.problem.dsol_test)
-        jac_form = dolfin.derivative(
-            res_form,
-            self.problem.sol_func,
-            self.problem.dsol_tria)
-        dolfin.assemble( #MG20190513: Cannot use point integral within assemble_system
-           -res_form,
-            tensor=self.res_vec,
-            add_values=True,
-            finalize_tensor=True,
-            form_compiler_parameters=self.problem.form_compiler_parameters)
-        timer = time.time() - timer
-        self.printer.print_str(" "+str(timer)+" s",tab=False,newline=False)
-        # self.printer.print_var("res_vec",self.res_vec.get_local())
-        dolfin.assemble( #MG20190513: Cannot use point integral within assemble_system
-            jac_form,
-            tensor=self.jac_mat,
-            add_values=True,
-            finalize_tensor=True,
-            form_compiler_parameters=self.problem.form_compiler_parameters)
-        timer = time.time() - timer
-        self.printer.print_str(" "+str(timer)+" s",tab=False)
-        # self.printer.print_var("jac_mat",self.jac_mat.array())
+            self.printer.print_str("Assembly (directional penalties)…",newline=False)
+            timer = time.time()
+            Pi = sum([(loading.val/2) * dolfin.inner(self.problem.subsols["U"].subfunc, loading.N)**2 * loading.measure for loading in self.problem.directional_penalties])
+            res_form = dolfin.derivative(
+                Pi,
+                self.problem.sol_func,
+                self.problem.dsol_test)
+            jac_form = dolfin.derivative(
+                res_form,
+                self.problem.sol_func,
+                self.problem.dsol_tria)
+            dolfin.assemble(
+               -res_form,
+                tensor=self.res_vec,
+                add_values=True,
+                finalize_tensor=True,
+                form_compiler_parameters=self.problem.form_compiler_parameters)
+            dolfin.assemble(
+                jac_form,
+                tensor=self.jac_mat,
+                add_values=True,
+                finalize_tensor=True,
+                form_compiler_parameters=self.problem.form_compiler_parameters)
+            timer = time.time() - timer
+            self.printer.print_str(" "+str(timer)+" s",tab=False)
+            # self.printer.print_var("res_vec",self.res_vec.get_local())
+            # self.printer.print_var("jac_mat",self.jac_mat.array())
+        else:
+            self.printer.print_str("Assembly…",newline=False)
+            timer = time.time()
+            dolfin.assemble_system(
+                self.problem.jac_form,
+               -self.problem.res_form,
+                bcs=[constraint.bc for constraint in self.constraints],
+                A_tensor=self.jac_mat,
+                b_tensor=self.res_vec,
+                add_values=False,
+                finalize_tensor=True,
+                form_compiler_parameters=self.problem.form_compiler_parameters)
+            timer = time.time() - timer
+            self.printer.print_str(" "+str(timer)+" s",tab=False)
+            # self.printer.print_var("res_vec",self.res_vec.get_local())
+            # self.printer.print_var("jac_mat",self.jac_mat.array())
 
         if not (numpy.isfinite(self.res_vec).all()):
             self.printer.print_str("Warning! Residual is NaN!")