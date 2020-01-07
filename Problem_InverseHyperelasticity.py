7f754e275aa2a23a91752b4317c1d14e4d5e8f26
Martin Genet <martin.genet@polytechnique.edu>
2020-01-07 17:26:35 +0100

    InverseHyperelasticityProblem now inherits from HyperelasticityProblem

--- Problem_InverseHyperelasticity.py
+++ Problem_InverseHyperelasticity.py
@@ -18,7 +18,7 @@ from .Problem import Problem
 
 ################################################################################
 
-class InverseHyperelasticityProblem(Problem):
+class InverseHyperelasticityProblem(HyperelasticityProblem):
 
 
 
@@ -29,75 +29,6 @@ class InverseHyperelasticityProblem(Problem):
 
         self.w_incompressibility = w_incompressibility
         assert (not (self.w_incompressibility)), "To do. Aborting."
-
-
-
-    def add_displacement_subsol(self,
-            degree):
-
-        self.add_vector_subsol(
-            name="U",
-            family="CG",
-            degree=degree)
-
-
-
-    def add_pressure_subsol(self,
-            degree):
-
-        if (degree == 0):
-            self.add_scalar_subsol(
-                name="P",
-                family="DG",
-                degree=0)
-        else:
-            self.add_scalar_subsol(
-                name="P",
-                family="CG",
-                degree=degree)
-
-
-
-    def set_subsols(self,
-            U_degree=1):
-
-        self.add_displacement_subsol(
-            degree=U_degree)
-
-        if (self.w_incompressibility):
-            self.add_pressure_subsol(
-                degree=U_degree-1)
-
-
-
-    def set_solution_degree(self,
-            U_degree=1): #MG20190513: Should have different name, right?
-
-        self.set_subsols(
-            U_degree=U_degree)
-        self.set_solution_finite_element()
-        self.set_solution_function_space()
-        self.set_solution_functions()
-
-        if (self.mesh.ufl_cell().cellname() in ("triangle", "tetrahedron")):
-            quadrature_degree = max(1, 2*(U_degree-1))
-        elif (self.mesh.ufl_cell().cellname() in ("quadrilateral", "hexahedron")):
-            quadrature_degree = max(1, 2*(self.dim*U_degree-1))
-        self.set_quadrature_degree(
-            quadrature_degree=quadrature_degree)
-
-        self.set_foi_finite_elements_DG(
-            degree=U_degree-1)
-        self.set_foi_function_spaces()
-
-
-
-    def get_displacement_function_space(self):
-
-        if (len(self.subsols) == 1):
-            return self.sol_fs
-        else:
-            return self.get_subsol_function_space(name="U")
 
 
 
@@ -114,36 +45,6 @@ class InverseHyperelasticityProblem(Problem):
         self.add_foi(expr=self.kinematics.Ee, fs=self.mfoi_fs, name="E")
         if (self.Q_expr is not None):
             self.add_foi(expr=self.kinematics.Ee_loc, fs=self.mfoi_fs, name="Ee_loc")
-
-
-
-    def set_materials(self,
-            elastic_behavior=None,
-            elastic_behavior_dev=None,
-            elastic_behavior_bulk=None,
-            subdomain_id=None):
-
-        if (self.w_incompressibility):
-            assert (elastic_behavior      is     None)
-            assert (elastic_behavior_dev  is not None)
-            assert (elastic_behavior_bulk is     None)
-        else:
-            assert  ((elastic_behavior      is not None)
-                or  ((elastic_behavior_dev  is not None)
-                and  (elastic_behavior_bulk is not None)))
-
-        subdomain = dcm.SubDomain(
-            problem=self,
-            elastic_behavior=elastic_behavior,
-            elastic_behavior_dev=elastic_behavior_dev,
-            elastic_behavior_bulk=elastic_behavior_bulk,
-            id=subdomain_id)
-
-        self.subdomains += [subdomain]
-
-        # self.add_foi(expr=subdomain.Sigma, fs=self.mfoi_fs, name="Sigma")
-        # self.add_foi(expr=subdomain.PK1  , fs=self.mfoi_fs, name="PK1"  )
-        self.add_foi(expr=subdomain.sigma, fs=self.mfoi_fs, name="sigma")
 
 
 
