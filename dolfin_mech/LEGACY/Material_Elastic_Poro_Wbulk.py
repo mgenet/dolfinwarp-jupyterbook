ba827afc25d6f9679d4b51bd6aa28d0279b037ca
Martin Genet <martin.genet@polytechnique.edu>
2022-04-05 15:25:45 +0200

    Archive Cécile's code

---
+++ dolfin_mech/LEGACY/Material_Elastic_Poro_Wbulk.py
@@ -0,0 +1,147 @@
+#coding=utf8
+
+################################################################################
+###                                                                          ###
+### Created by Martin Genet, 2018-2022                                       ###
+###                                                                          ###
+### École Polytechnique, Palaiseau, France                                   ###
+###                                                                          ###
+###                                                                          ###
+### And Cécile Patte, 2019-2021                                              ###
+###                                                                          ###
+### INRIA, Palaiseau, France                                                 ###
+###                                                                          ###
+################################################################################
+
+import dolfin
+
+import dolfin_mech as dmech
+from .Material_Elastic_Bulk import BulkElasticMaterial
+
+################################################################################
+
+class SkeletonPoroBulkElasticMaterial(BulkElasticMaterial):
+
+
+
+    def __init__(self,
+            problem,
+            parameters):
+
+        self.problem = problem
+
+        assert ('kappa' in parameters)
+        self.kappa = dolfin.Constant(parameters['kappa'])
+
+
+
+    def get_dWbulkdJs(self, Phi0, Phi):
+
+        Js = self.problem.kinematics.Je * (1 - Phi)
+        dWbulkdJs = self.kappa * (1. / (1. - Phi0) - 1./Js)
+        # dWbulkdJs = self.kappa * (1. / (1. - Phi0) - 1./self.problem.kinematics.Js)
+        dWbulkdJs = (1 - Phi0) * dWbulkdJs
+        # dWbulkdJs = (1 - self.problem.Phi0) * dWbulkdJs
+
+        return dWbulkdJs
+
+
+
+###################################################### for mixed formulation ###
+
+    def get_res_term(self, Phi0, Phi, w_U = None, w_Phi0 = None, w_Phi = None):
+
+        assert (w_U is     None) or (w_Phi0 is     None) or (w_Phi is     None)
+        assert (w_U is not None) or (w_Phi0 is not None) or (w_Phi is not None)
+
+        dWbulkdJs = self.get_dWbulkdJs(Phi0, Phi)
+
+        if w_U is not None:
+            if isinstance(self.problem, dmech.InverseHyperelasticityProblem):
+                res_form = dolfin.inner(
+                    dWbulkdJs * self.problem.kinematics.I,
+                    dolfin.sym(dolfin.grad(self.problem.get_displacement_subsol().dsubtest))) * self.problem.dV
+
+            elif isinstance(self.problem, dmech.HyperelasticityProblem):
+                res_form = dolfin.inner(
+                    dWbulkdJs * self.problem.kinematics.Je * self.problem.kinematics.Ce_inv,
+                    dolfin.derivative(
+                            self.problem.kinematics.Et,
+                            self.problem.get_displacement_subsol().subfunc,
+                            self.problem.get_displacement_subsol().dsubtest)) * self.problem.dV
+
+        elif w_Phi0 is not None:
+            res_form = dolfin.inner(
+                dWbulkdJs,
+                self.problem.subsols["Phi0"].dsubtest) * self.problem.dV
+
+        elif w_Phi is not None:
+            res_form = dolfin.inner(
+                dWbulkdJs,
+                self.problem.subsols["Phi"].dsubtest) * self.problem.dV
+
+        return res_form
+
+
+
+########################################## for internal variable formulation ###
+
+    def get_jac_term(self, Phi0, Phi, w_Phi0 = None, w_Phi = None):
+
+        assert (w_Phi0 is     None) or (w_Phi is     None)
+        assert (w_Phi0 is not None) or (w_Phi is not None)
+
+        dWbulkdJs = self.get_dWbulkdJs(Phi0, Phi)
+
+        if w_Phi is not None:
+
+            # Phi = self.problem.get_Phi()
+            # if self.problem.w_contact:
+            #     dWbulkdJs = self.get_dWbulkdJs(self.problem.Phi0pos, self.problem.Phipos)
+            # else:
+            #     dWbulkdJs = self.get_dWbulkdJs(self.problem.Phi0, Phi)
+            #     # dWbulkdJs = self.get_dWbulkdJs(self.problem.Phi0, self.problem.Phi)
+            #     # dWbulkdJspos = self.get_dWbulkdJs(self.problem.Phi0pos, self.problem.Phipos)
+
+            jac_form = dolfin.inner(
+                dolfin.diff(
+                    dWbulkdJs * self.problem.kinematics.Je * self.problem.kinematics.Ce_inv,
+                    # dWbulkdJspos * self.problem.kinematics.Je * self.problem.kinematics.Ce_inv,
+                    self.problem.Phi),
+                dolfin.derivative(
+                    self.problem.kinematics.Et,
+                    self.problem.get_displacement_subsol().subfunc,
+                    self.problem.get_displacement_subsol().dsubtest)) * dolfin.inner(
+                dolfin.diff(
+                    self.problem.get_Phi(),
+                    dolfin.variable(self.problem.kinematics.Jt)) * dolfin.diff(
+                    self.problem.kinematics.Jt,
+                    dolfin.variable(self.problem.kinematics.Ft)),
+                dolfin.derivative(
+                    self.problem.kinematics.Ft,
+                    self.problem.get_displacement_subsol().subfunc,
+                    self.problem.get_displacement_subsol().dsubtria)) * self.problem.dV
+
+        elif w_Phi0 is not None:
+
+            jac_form = dolfin.inner(
+                dolfin.diff(
+                    dWbulkdJs * self.problem.kinematics.I,
+                    # dWbulkdJs * self.problem.kinematics.Je * self.problem.kinematics.Ce_inv,
+                    # dWbulkdJspos * self.problem.kinematics.Je * self.problem.kinematics.Ce_inv,
+                    self.problem.Phi0),
+                dolfin.derivative(
+                    self.problem.kinematics.Et,
+                    self.problem.get_displacement_subsol().subfunc,
+                    self.problem.get_displacement_subsol().dsubtest)) * dolfin.inner(
+                dolfin.diff(
+                    self.problem.get_Phi0(),
+                    dolfin.variable(self.problem.kinematics.Jt)) * dolfin.diff(
+                    self.problem.kinematics.Jt,
+                    dolfin.variable(self.problem.kinematics.Ft)),
+                dolfin.derivative(
+                    self.problem.kinematics.Ft,
+                    self.problem.get_displacement_subsol().subfunc,
+                    self.problem.get_displacement_subsol().dsubtria)) * self.problem.dV
+
+        return jac_form
---
+++ dolfin_mech/LEGACY/Material_Elastic_Poro_Wpor.py
@@ -0,0 +1,98 @@
+#coding=utf8
+
+################################################################################
+###                                                                          ###
+### Created by Martin Genet, 2018-2022                                       ###
+###                                                                          ###
+### École Polytechnique, Palaiseau, France                                   ###
+###                                                                          ###
+###                                                                          ###
+### And Cécile Patte, 2019-2021                                              ###
+###                                                                          ###
+### INRIA, Palaiseau, France                                                 ###
+###                                                                          ###
+################################################################################
+
+import dolfin
+
+import dolfin_mech as dmech
+from .Material_Elastic import ElasticMaterial
+
+################################################################################
+
+class WporPoroElasticMaterial(ElasticMaterial):
+
+
+
+    def __init__(self,
+            problem,
+            parameters,
+            type):
+
+        self.problem = problem
+
+        assert ('eta' in parameters)
+        self.eta = parameters['eta']
+        self.n   = parameters['n']
+
+        assert ((type in ['inverse','exp']) or isinstance(type, int))
+        self.type = type
+
+
+
+    def get_dWpordJs(self):
+        # Jf = self.kinematics.Je - self.kinematics.Js
+        Jf = self.problem.kinematics.Je * self.problem.Phi
+        if (self.type == 'inverse'): # inverse problem
+            dWpor1dJs =   self.eta * (self.n+1) * ((self.problem.Phi0 - Jf) / Jf)**self.n * self.problem.Phi0 / (Jf)**2
+            dWpor2dJs = - self.eta * (self.n+1) * ((Jf / self.problem.Phi0) - 1.)**self.n / self.problem.Phi0
+            dWpordJs  = dolfin.conditional(dolfin.lt(self.problem.Phi, self.problem.Phi0), dWpor1dJs, dWpor2dJs)
+        elif (self.type == 'exp'):
+            dWpordJs = self.eta * (self.problem.Phi0 / (Jf))**2 * dolfin.exp(-Jf / (self.problem.Phi0 - Jf)) / (self.problem.Phi0 - Jf)
+            dWpordJs = dolfin.conditional(dolfin.lt(self.problem.Phi, self.problem.Phi0), dWpordJs, 0)
+        elif (isinstance(self.type, int)):
+            if (self.type == 2):
+                dWpordJs = self.eta * self.n * ((self.problem.Phi0 - Jf) / Jf)**(self.n-1) * self.problem.Phi0 / (Jf)**2
+                dWpordJs = dolfin.conditional(dolfin.lt(self.problem.Phi, self.problem.Phi0), dWpordJs, 0)
+        dWpordJs = (1. - self.problem.Phi0) * dWpordJs
+
+        return dWpordJs
+
+
+
+###################################################### for mixed formulation ###
+
+    def get_res_term(self,
+			w_Phi0=None,
+			w_Phi=None):
+
+        assert ((w_Phi0 is     None) or (w_Phi is     None))
+        assert ((w_Phi0 is not None) or (w_Phi is not None))
+
+        dWpordJs = self.get_dWpordJs()
+
+        if (w_Phi0 is not None):
+            res_form = dolfin.inner(
+				dWpordJs,
+				self.problem.subsols["Phi0"].dsubtest) * self.problem.dV
+        elif (w_Phi is not None):
+            res_form = dolfin.inner(
+				dWpordJs,
+				self.problem.subsols["Phi"].dsubtest) * self.problem.dV
+
+        return res_form
+
+
+
+########################################## for internal variable formulation ###
+
+    def get_jac_term(self, w_Phi0 = None, w_Phi = None):
+
+        assert (w_Phi0 is     None) or (w_Phi is     None)
+        assert (w_Phi0 is not None) or (w_Phi is not None)
+
+        # dWpordJs = self.get_dWpordJs()
+
+        assert(0)
+
+        return jac_form
